43a055fb3c36e1a9fe035834d85c7ac24c42e6a7
M_Farsith <98216395+ff786@users.noreply.github.com>
2026-09-21 03:55:44 +0530

    refactor: isolate ScrollManager from app bootstrap

--- src/Main.jsx
+++ src/Main.jsx
@@ -14,51 +14,7 @@ import "./styles/typography.css";
 import "./styles/global.css";
 import "./styles/components.css";
 
-const scrollToHash = () => {
-    const hash = window.location.hash;
-
-    if (!hash) {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        return;
-    }
-
-    const target = document.querySelector(hash);
-
-    if (!target) return;
-
-    const headerOffset = window.innerWidth <= 767 ? 72 : 88;
-    const top =
-        target.getBoundingClientRect().top +
-        window.scrollY -
-        headerOffset;
-
-    window.scrollTo({
-        top,
-        behavior: "smooth",
-    });
-};
-
-const ScrollManager = () => {
-    React.useEffect(() => {
-        const handleRouteReady = () => {
-            requestAnimationFrame(() => {
-                requestAnimationFrame(scrollToHash);
-            });
-        };
-
-        window.addEventListener("popstate", handleRouteReady);
-        window.addEventListener("hashchange", handleRouteReady);
-
-        handleRouteReady();
-
-        return () => {
-            window.removeEventListener("popstate", handleRouteReady);
-            window.removeEventListener("hashchange", handleRouteReady);
-        };
-    }, []);
-
-    return null;
-};
+import ScrollManager from "./components/common/ScrollManager";
 
 initLenis();
 
