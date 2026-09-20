56a313b83aea923869dc2c2d49d557abe3a05cb6
M_Farsith <98216395+ff786@users.noreply.github.com>
2026-09-21 03:28:29 +0530

    feat: mount SEO and cookie consent globally

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,10 +3,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import WhatsAppChatButton from "../common/WhatsAppChatButton";
+import CookieConsent from "../common/CookieConsent";
+import SEO from "../common/SEO";
 
 const Layout = () => {
     return (
         <div className="nuvia-app">
+            <SEO />
             <Header />
 
             <main className="nuvia-main">
@@ -16,6 +19,7 @@ const Layout = () => {
             <Footer />
 
             <WhatsAppChatButton />
+            <CookieConsent />
         </div>
     );
 };
